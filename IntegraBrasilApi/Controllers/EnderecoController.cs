d8a0263f87748b4f326ddf2ea0fe4e54f7585789
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-26 21:19:16 -0300

    Controller - Criação de EnderecoControler

---
+++ IntegraBrasilApi/Controllers/EnderecoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegraBrasilApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+    }
+}
