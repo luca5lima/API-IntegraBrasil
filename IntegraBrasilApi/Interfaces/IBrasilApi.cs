af77c4318dc7cd08ced164b83734af5ff90d4fb2
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-27 20:36:46 -0300

    Interfaces - Implementação de IBrasilApi

--- IntegraBrasilApi/Interfaces/IBrasilApi.cs
+++ IntegraBrasilApi/Interfaces/IBrasilApi.cs
@@ -1,6 +1,13 @@
-﻿namespace IntegraBrasilApi.Interfaces
+﻿using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Models;
+
+namespace IntegraBrasilApi.Interfaces
 {
     public interface IBrasilApi
     {
+
+        Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep);
+        Task<ResponseGenerico<List<EnderecoModel>>> BuscarTodosBancos();
+        Task<ResponseGenerico<EnderecoModel>> BuscarBanco(string codigoBanco);
     }
 }
---
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -0,0 +1,24 @@
+﻿using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Interfaces;
+using IntegraBrasilApi.Models;
+
+namespace IntegraBrasilApi.Rest
+{
+    public class BrasilApiRest : IBrasilApi
+    {
+        public Task<ResponseGenerico<EnderecoModel>> BuscarBanco(string codigoBanco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseGenerico<List<EnderecoModel>>> BuscarTodosBancos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
