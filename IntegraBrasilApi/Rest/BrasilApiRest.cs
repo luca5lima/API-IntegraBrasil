750d69e6100eb8afc20684d2675a22da3c2ffd7c
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-27 21:10:37 -0300

    Rest - Implementação metodo BuscarEnderecoPorCep

--- IntegraBrasilApi/Rest/BrasilApiRest.cs
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -1,4 +1,6 @@
-﻿using IntegraBrasilApi.Dtos;
+﻿using System.Dynamic;
+using System.Text.Json;
+using IntegraBrasilApi.Dtos;
 using IntegraBrasilApi.Interfaces;
 using IntegraBrasilApi.Models;
 
@@ -6,19 +8,42 @@ namespace IntegraBrasilApi.Rest
 {
     public class BrasilApiRest : IBrasilApi
     {
-        public Task<ResponseGenerico<EnderecoModel>> BuscarBanco(string codigoBanco)
-        {
-            throw new NotImplementedException();
-        }
+        public object JsonSerializar { get; private set; }
 
-        public Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep)
+        public async Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://brasilapi.com.br/api/cep/v1/{cep}");
+
+            var response = new ResponseGenerico<EnderecoModel>();
+            using (var client = new HttpClient())
+            {
+                var responseBrasilApi = await client.SendAsync(request);
+                var contentResp = await responseBrasilApi.Content.ReadAsStringAsync();
+                var objResponser = JsonSerializer.Deserialize<EnderecoModel>(contentResp);
+
+                if (responseBrasilApi.IsSuccessStatusCode)
+                {
+                    response.CodigoHttps = responseBrasilApi.StatusCode;
+                    response.DadosRetorno = objResponser;
+                }
+                else
+                {
+                    response.CodigoHttps = responseBrasilApi.StatusCode;
+                    response.ErroRetorno = JsonSerializer.Deserialize<ExpandoObject>(contentResp);
+                }
+
+                return response;
+            }
         }
 
         public Task<ResponseGenerico<List<EnderecoModel>>> BuscarTodosBancos()
         {
             throw new NotImplementedException();
+        }
+
+        public Task<ResponseGenerico<EnderecoModel>> BuscarBanco(string codigoBanco)
+        {
+            
         }
     }
 }
