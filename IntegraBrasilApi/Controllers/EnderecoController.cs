e5b7466d9415ead0603c0b52f897c45f8ddd13a4
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-28 23:20:48 -0300

    Controller - Implementando EnderecoController

--- IntegraBrasilApi/Controllers/EnderecoController.cs
+++ IntegraBrasilApi/Controllers/EnderecoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using IntegraBrasilApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IntegraBrasilApi.Controllers
 {
@@ -6,5 +8,30 @@ namespace IntegraBrasilApi.Controllers
     [Route("api/v1/[controller]")]
     public class EnderecoController : ControllerBase
     {
+        public readonly IEnderecoService _enderecoService;
+
+        public EnderecoController(IEnderecoService enderecoService) 
+        {
+            _enderecoService = enderecoService;
+        }
+
+        [HttpGet("busca/{cep}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> BuscarEndereco([FromRoute] string cep)
+        {
+            var response = await _enderecoService.BuscarEndereco(cep);
+
+            if (response.CodigoHttp == HttpStatusCode.OK)
+            {
+                return Ok(response.DadosRetorno);
+            }
+            else
+            {
+                return StatusCode((int)response.CodigoHttp, response.ErroRetorno);
+            }
+        }
     }
 }
