0ac16b4c4c79c892632d5c46e24142510720516d
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-28 23:32:29 -0300

    Rest - Implementando rota de BuscarTodosBancos - Brasil API

--- IntegraBrasilApi/Rest/BrasilApiRest.cs
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Dynamic;
+using System.Runtime.ConstrainedExecution;
 using System.Text.Json;
 using IntegraBrasilApi.Dtos;
 using IntegraBrasilApi.Interfaces;
@@ -35,9 +36,30 @@ namespace IntegraBrasilApi.Rest
             }
         }
 
-        public Task<ResponseGenerico<List<BancoModel>>> BuscarTodosBancos()
+        public async Task<ResponseGenerico<List<BancoModel>>> BuscarTodosBancos()
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://brasilapi.com.br/api/banks/v1");
+
+            var response = new ResponseGenerico<List<BancoModel>>();
+            using (var client = new HttpClient())
+            {
+                var responseBrasilApi = await client.SendAsync(request);
+                var contentResp = await responseBrasilApi.Content.ReadAsStringAsync();
+                var objResponser = JsonSerializer.Deserialize<BancoModel>(contentResp);
+
+                if (responseBrasilApi.IsSuccessStatusCode)
+                {
+                    response.CodigoHttp = responseBrasilApi.StatusCode;
+                    response.DadosRetorno = objResponser;
+                }
+                else
+                {
+                    response.CodigoHttp = responseBrasilApi.StatusCode;
+                    response.ErroRetorno = JsonSerializer.Deserialize<ExpandoObject>(contentResp);
+                }
+
+                return response;
+            }
         }
 
         public Task<ResponseGenerico<BancoModel>> BuscarBanco(string codigoBanco)
