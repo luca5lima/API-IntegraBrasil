31afdcbc32ad5dd27a13cde6f6f75c4c68a4851d
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-28 23:16:46 -0300

    Mappingins - Configurando AutoMapper

---
+++ IntegraBrasilApi/Mappings/EnderecoMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Models;
+
+namespace IntegraBrasilApi.Mapping
+{
+    public class EnderecoMapping : Profile
+    {
+        public EnderecoMapping()
+        {
+            CreateMap(typeof(ResponseGenerico<>), typeof(ResponseGenerico<>));
+            CreateMap<EnderecoResponse, EnderecoModel>();
+            CreateMap<EnderecoModel, EnderecoResponse>();
+        }
+    }
+}
