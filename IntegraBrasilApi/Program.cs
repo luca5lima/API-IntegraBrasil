e4ebd28da377b2fc303099681ab73cd7903ed9a7
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-28 23:19:04 -0300

    Program - Injetando Serviços

--- IntegraBrasilApi/Program.cs
+++ IntegraBrasilApi/Program.cs
@@ -1,25 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using IntegraBrasilApi.Interfaces;
+using IntegraBrasilApi.Mapping;
+using IntegraBrasilApi.Rest;
+using IntegraBrasilApi.Services;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IEnderecoService, EnderecoService>();
+        //builder.Services.AddSingleton<IBancoServices, BancoServices>();
+        builder.Services.AddSingleton<IBrasilApi, BrasilApiRest>();
+
+        builder.Services.AddAutoMapper(typeof(EnderecoMapping));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
