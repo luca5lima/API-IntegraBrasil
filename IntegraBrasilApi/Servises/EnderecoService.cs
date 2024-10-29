f46114be0fe3966532acfe77c670c472549492f6
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-28 23:13:14 -0300

    Service - Implementando EnderecoService

--- IntegraBrasilApi/Servises/EnderecoService.cs
+++ IntegraBrasilApi/Servises/EnderecoService.cs
@@ -1,6 +1,25 @@
-﻿namespace IntegraBrasilApi.Servises
+﻿using AutoMapper;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Interfaces;
+
+namespace IntegraBrasilApi.Services
 {
-    public class EnderecoService
+    public class EnderecoService : IEnderecoService
     {
+        private readonly IMapper _mapper;
+        private readonly IBrasilApi _brasilApi;
+        
+        public EnderecoService(IMapper mapper, IBrasilApi brasilApi)
+        {
+            _mapper = mapper;
+            _brasilApi = brasilApi;
+        }
+
+        public async Task<ResponseGenerico<EnderecoResponse>> BuscarEndereco(string cep)
+        {
+            var endereco = await _brasilApi.BuscarEnderecoPorCEP(cep);
+
+            return _mapper.Map<ResponseGenerico<EnderecoResponse>>(endereco);
+        }
     }
 }
